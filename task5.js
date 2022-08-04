c22647d3e977fd77b5c0c9564e95dd619ab63a10
peeyushdhanger <peeyushdhanger@gmail.com>
2022-08-04 11:00:46 +0530

    dom1

---
+++ task5.js
@@ -0,0 +1,12 @@
+//GET ELEMENT BY TAG NAME
+var li = document.getElementsByTagName('li');
+console.log(li);
+//console.log(li[4]);
+li[4].textContent = 'item5';
+//li[4].style.fontWeight = 'bold';
+li[4].style.backgroundColor = 'yellow';
+li[4].style.border = '2px solid grey';
+li[4].style.padding = '10px';
+li[4].style.margin = '2px';
+
+//GET ELEMENTBYCLASS NAME
